6ebf9a653b30ede116c4cc99b6793e9200a9e841
gabrielh-muniz <gabrielhmuniiz@gmail.com>
2025-11-25 09:19:17 -0300

    fix: change the interaction return statement and add the keyword await to it

--- src/lib/safe-reply.js
+++ src/lib/safe-reply.js
@@ -3,9 +3,9 @@ import { logger } from "./logger.lib.js";
 export async function safeReply(interaction, message) {
   try {
     if (interaction.replied || interaction.deferred) {
-      return interaction.followUp(message);
+      return await interaction.followUp(message);
     }
-    return interaction.reply(message);
+    return await interaction.reply(message);
   } catch (error) {
     logger.error("Error sending reply:", error);
   }
